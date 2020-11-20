c61d732ef8f18d2b7611f12a79c64b1cda90ef58
nagrom44 <56819836+nagrom44@users.noreply.github.com>
2020-11-20 15:43:06 +0100

    Add files via upload

---
+++ filmNMMF/src/main/java/fr/eni/filmNMMF/FilmNmmfApplication.java
@@ -0,0 +1,13 @@
+package fr.eni.filmNMMF;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilmNmmfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilmNmmfApplication.class, args);
+	}
+
+}
---
+++ filmNMMF/src/test/java/fr/eni/filmNMMF/FilmNmmfApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.eni.filmNMMF;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilmNmmfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
